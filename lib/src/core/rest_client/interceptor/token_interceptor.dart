1c6f3949946a4e946ef59dd5121422b6f7ee2009
Johnathan Rocha <johnathanrocha@gmail.com>
2024-10-12 00:26:48 -0300

    feat: handle with token interceptor

--- lib/src/core/rest_client/interceptor/token_interceptor.dart
+++ lib/src/core/rest_client/interceptor/token_interceptor.dart
@@ -1,11 +1,23 @@
 import 'package:dio/dio.dart';
+import 'package:poc_minsait/src/core/device/app_storage.dart';
 
 class TokenInterceptor extends Interceptor {
+  final AppSecureStorage _appSecureStorage;
+
+  TokenInterceptor({required AppSecureStorage appSecureStorage})
+      : _appSecureStorage = appSecureStorage;
+
   @override
   void onRequest(
     RequestOptions options,
     RequestInterceptorHandler handler,
-  ) {
-    handler.next(options);
+  ) async {
+    final token = await _appSecureStorage.getSessionToken();
+
+    if (token != null) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+
+    return handler.next(options);
   }
 }
